ef0040615a6079d389bfaf5529585e56392c406e
Fábio Lucena Ribas <fabio.luce@outlook.com>
2022-01-02 08:08:12 -0300

    Removendo css não utilizado e ajustando teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { TableModule } from 'primeng/table';
-import { DialogModule } from 'primeng/dialog';
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            imports: [
-                FormsModule,
-                HttpClientTestingModule,
-                TableModule,
-                DialogModule
-            ],
+            imports: [],
             declarations: [
                 AppComponent
             ],
         }).compileComponents();
     }));
 
-    it('should create the app', async(() => {
+    it('should create the app', waitForAsync(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
